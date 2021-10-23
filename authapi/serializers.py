77b27402d81202c593c315e9ecfd1ee7cd738c0c
Gabriel Sana <gabriel.sana@digitalsys.com.br>
2021-10-23 13:21:07 -0300

    api finalizada

--- authapi/serializers.py
+++ authapi/serializers.py
@@ -5,7 +5,7 @@ from .models import *
 class UserCreateSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
         model = User
-        fields = ('id','username','email','user_type','first_name','last_name','password')
+        fields = ('id','username','email','user_type','first_name','last_name','password','is_superuser','is_staff')
 
 class UserViewSerializer(UserSerializer):
     class Meta(UserSerializer.Meta):
--- authapi/views.py
+++ authapi/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import *
+from .models import *
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserViewSerializer
