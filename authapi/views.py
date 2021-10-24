0114ecc99160159716b1de8af25efab08e9ae536
Gabriel Sana <gabriel.sana@digitalsys.com.br>
2021-10-24 20:35:26 -0300

    adicionando variavel de controle de infos pessoais

--- authapi/views.py
+++ authapi/views.py
@@ -7,4 +7,4 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserViewSerializer
     filter_backends = (filters.DjangoFilterBackend,)
-    filterset_fields = ('id','username','email','user_type','first_name','last_name','date_joined')
+    filterset_fields = ('id','username','email','user_type','first_name','last_name','info_cadastrada','date_joined')
